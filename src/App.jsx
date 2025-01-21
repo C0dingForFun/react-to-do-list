b96f300d4a886c48486ef1d32ee5d9a2e7446c93
C0dingForFun <gwibantulini8@gmail.com>
2025-01-21 11:11:38 +0200

    added modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,19 @@
 import { useState } from "react";
+import React from "react";
 import "./styles.css";
-
+import Modal from "./Modal.jsx"
 export default function App(){
   const [newItem, setNewItem] = useState("")
   const [todos, setTodos] = useState([]);
+  const [modal, setModal] = React.useState(false)
+
+  function handleClose(){
+    setModal(false);
+  }
+
+  function handleOpen(){
+    setModal(true)
+  }
 
   function handleSubmit(e){
     e.preventDefault() //this prevents the page from refreshing.
@@ -14,16 +24,16 @@ export default function App(){
         {id: crypto.randomUUID(), title: newItem, completed:false}
       ]
     })
-
+    
     setNewItem("");
   }
-
- function toggleTodo(id, completed){
-   setTodos((currentTodos)=>{
-     return currentTodos.map((todo)=>{
-       if(todo.id === id){
-         return {...todo, completed}
-       }
+  
+  function toggleTodo(id, completed){
+    setTodos((currentTodos)=>{
+      return currentTodos.map((todo)=>{
+        if(todo.id === id){
+          return {...todo, completed}
+        }
        return todo
       })
     })
@@ -35,7 +45,6 @@ export default function App(){
     })
   }
   
-
   return (
   <>
   <div className="container">
@@ -44,25 +53,31 @@ export default function App(){
     <form onSubmit={handleSubmit} className="new-item-form my-2"> 
       <div className="form row">
           <label htmlFor="item text-center"><h2>New Item</h2></label>
-          {/* <div> */}
-            <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" className = "my-2" placeholder="~ Do the dishes"/>
-            {/* <select > */}
-              {/* <option>Select Task Priority</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-              <option value="3">3</option>
-              <option value="4">4</option>
-              <option value="5">5</option>
-            </select>
-            <input type="text" /> */}
-          {/* </div> */}
-          <button className="addBtn btn-success">Add Item</button>
+            <div className="row mt-5">
+            <div className="col-6">
+              <input value={newItem} onChange={e => setNewItem(e.target.value)} type="text" id="item" className = "my-2" placeholder="~ Do the dishes"/>
+            </div>
+            <div className="col-6">
+              <select className="mx-2">
+                <option>Select Task Priority</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </select>
+            </div>
+            </div>
+          <div>
+            <textarea placeholder="Task Description" className=""></textarea>
+          </div>
+          <button className="addBtn">Add Item</button>
       </div>
     </form>
     </div>
     <div className="my-2">
       <h1 className="header my-5">To-Dos</h1>
-      <ul className="list my-5">
+      <ul className="list my-4">
         {/* this is where we will map over the todos array and display each item; and anything inside braces will run as javascript code*/}
         {todos.length === 0 && "No To-Dos Yet"}
         {todos.map(todo =>{
@@ -73,7 +88,14 @@ export default function App(){
                 {todo.title}
               </label>
               <span>&#10031;</span>
-              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger mx-1">Delete</button>
+              <button className="btn btn-primary" onClick={handleOpen}>View Description</button>
+              <Modal isOpen={modal} onClose={handleClose}>
+                <>
+                  <h1>testing</h1>
+                </>
+              </Modal>
+              <button className="btn btn-success mx-1">Edit</button>
+              <button onClick={() => deleteTodo(todo.id)} className="btn btn-danger">Delete</button>
             </li>
           )
         })}  
---
+++ src/Modal.jsx
@@ -0,0 +1,42 @@
+const Modal = ({ isOpen, onClose, children }) => {
+    if (!isOpen) return null;
+
+    return (
+        <div
+            onClick={onClose}
+            style={{
+                position: "fixed",
+                top: 0,
+                left: 0,
+                width: "100%",
+                height: "100%",
+                background: "rgba(0, 0, 0, 0.5)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+            }}
+        >
+            <div
+                style={{
+                    position: "absolute",
+                    border: "2px solid #000",
+                    borderRadius: "20px",
+                    backgroundColor: "rgb(0,0,0,0.5)",
+                    boxShadow: "2px solid black",
+                    height: 150,
+                    width: 240,
+                    margin: "auto",
+                    padding: "2%",
+                    color: "white",
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center"
+                }}
+            >
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default Modal;
